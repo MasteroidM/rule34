0e6c7fc0d60708f664fff0aaf39ec3ada00bd0f4
Melwin Lewis <74094974+MasteroidM@users.noreply.github.com>
2025-03-01 12:20:15 +0530

    Update test.py -> MODEL_PATH

--- Mocap/test.py
+++ Mocap/test.py
@@ -9,7 +9,7 @@ mp_drawing = mp.solutions.drawing_utils
 hands = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
 # Load EfficientDet-Lite0 TFLite Model
-MODEL_PATH = "D:/Hackathon/Mocap/venv/Models/efficientdet_lite0.tflite"
+MODEL_PATH = "./venv/Models/efficientdet_lite0.tflite"
 
 try:
     interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
